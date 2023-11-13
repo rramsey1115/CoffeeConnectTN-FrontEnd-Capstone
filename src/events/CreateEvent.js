90fa14690a8c337480f53d7c2f2cdc321bbe6353
Ryan Ramsey <rramsey1115@gmail.com>
2023-11-13 13:58:08 -0600

    edited datepicker

--- src/events/CreateEvent.js
+++ src/events/CreateEvent.js
@@ -29,7 +29,7 @@ export const CreateEvent = ({ currentUser, userLocation }) => {
     const date = new Date(dateString); // {object Date}
     const yyyy = date.getFullYear();
     let mm = date.getMonth() + 1;
-    let dd = date.getDate();
+    let dd = date.getDate() + 1;
     if (dd < 10) dd = "0" + dd;
     if (mm < 10) mm = "0" + mm;
     const formatted = yyyy + "-" + mm + "-" + dd;
